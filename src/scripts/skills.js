c4d667fd31524f23eec589509c606babbcbdc63b
XeniaBuerki <xenia1898@gmail.com>
2020-05-15 00:29:55 +0700

    Add interactive drawing of skills with Vue-components

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,6 +1,43 @@
 import Vue from "vue";
 
+const skill = {
+  template: "#skill",
+  props: [
+    "skill"
+  ],
+  methods: {
+    drawSkillsCircle() {
+      const circle = this.$refs["circle"]
+      circle.style.animationDelay = this.skill.percent;
+    }
+  },
+  mounted() {
+    this.drawSkillsCircle()
+  }
+}
+
+const skillsGroup = {
+  template: "#skills-group",
+  components: {
+    skill
+  },
+  props: [
+    "category"
+  ]
+}
+
 new Vue({
-        el: "#skills-values",
-        template: "#skills-list"
-    });
+  el: "#skills-values",
+  template: "#skills-list",
+  components: {
+    skillsGroup, skill
+  },
+  data() {
+    return {
+      skills: []
+    }
+  },
+  created() {
+    this.skills = require('../data/skills.json');
+  }
+});
